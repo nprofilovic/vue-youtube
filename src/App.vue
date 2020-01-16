c4048b8fd31fe2ae8611bfdfbd4bd137a7a7f815
nprofilovic <nprofilovic@gmail.com>
2020-01-16 17:28:09 +0100

    Make new componetn Video List

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <SearchBar @termChange="onTermChange"></SearchBar>
+    <SearchBar @termChange="onTermChange" />
+    <VideoList />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+const API_KEY = 'AIzaSyCbO3Z1BAuLheDTh-T_DsnO1E4CbP0at5Q'
+
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
   },
 
   methods: {
-    onTermChange(searchTerm) {
-      
-      console.log(searchTerm);
-
-    }
+    onTermChange(searchTerm) {  
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      })
+      .then(response => console.log(response))
+     }
   }
   
 }
---
+++ src/components/VideoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      Video List
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoList'
+}
+</script>
+
+<style>
+
+</style>
