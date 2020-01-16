4101e7f9f392fa68dd8c9ccd486e15c15e468043
nprofilovic <nprofilovic@gmail.com>
2020-01-16 14:03:42 +0100

    Make Search component

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+        'vue/singleline-html-element-content-newline': 0,
+        'vue/max-attributes-per-line': 0,
+        'no-console': 0
+    }
+}
